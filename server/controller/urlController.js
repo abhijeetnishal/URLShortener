41070659b3ed4d4878b6bbee09eeec6554a93bb8
abhijeetnishal <abhijeetnishal@gmail.com>
2024-05-08 21:15:33 +0530

    fix: Hardcoded Redirect URL

--- server/controller/urlController.js
+++ server/controller/urlController.js
@@ -1,76 +1,72 @@
-const urlModel = require('../model/urlSchema');
-const validUrl = require('valid-url');
-const uniqueString = require('../utils/utils')
+const urlModel = require("../model/urlSchema");
+const validUrl = require("valid-url");
+const uniqueString = require("../utils/utils");
+const dotenv = require("dotenv");
 
- 
+dotenv.config();
+
 const getSpecificUrl = async (req, res) => {
-    try{
-        //receives json data from client.
-        const shortId = req.params.shortId;
+  try {
+    //receives json data from client.
+    const shortId = req.params.shortId;
 
-        //get original url from unique key as it is mapped with original url in redis.
-        const urlData = await urlModel.findOne({shortId: shortId});
-        const originalUrl = urlData.originalUrl;
+    //get original url from unique key as it is mapped with original url in redis.
+    const urlData = await urlModel.findOne({ shortId: shortId });
+    const originalUrl = urlData.originalUrl;
 
-        //if original url is present in redis then redirect it.
-        if(originalUrl) {
-            res.redirect(originalUrl);
-        } 
-        else{
-            res.status(404).send('URL not found');
-        }
+    //if original url is present in redis then redirect it.
+    if (originalUrl) {
+      res.redirect(originalUrl);
+    } else {
+      res.status(404).send("URL not found");
     }
-    catch(error){
-        console.log(error);
-        res.status(500).json({message:"Internal Server Error"});
-    }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 const createUrl = async (req, res) => {
-    //receives json data from client
-    const originalUrl = req.body.originalUrl;
+  //receives json data from client
+  const originalUrl = req.body.originalUrl;
 
-    //validate the original url received from client
-    if(validUrl.isUri(originalUrl)){
-        try{
-            //check if url already shorted or not.
-            const urlExist = await urlModel.findOne({originalUrl: originalUrl});
+  //validate the original url received from client
+  if (validUrl.isUri(originalUrl)) {
+    try {
+      //check if url already shorted or not.
+      const urlExist = await urlModel.findOne({ originalUrl: originalUrl });
 
-            if(urlExist){
-                //If exist then simply return the shortId instead of creating new one
-                const shortId = urlExist.shortId;
-                res.status(201).json(`http://localhost:4000/${shortId}`);
-            }
+      if (urlExist) {
+        //If exist then simply return the shortId instead of creating new one
+        const shortId = urlExist.shortId;
+        res.status(201).json(`${process.env.REDIRECT_URL}/${shortId}`);
+      } else {
+        //generate 7 digit unique code using base-62 encoding
+        const shortId = uniqueString.generateBase62String();
+        //store data(shorturl, longurl)
+        const newUrl = new urlModel({
+          originalUrl: originalUrl,
+          shortId: shortId,
+        });
 
-            else{
-                //generate 7 digit unique code using base-62 encoding
-                const shortId = uniqueString.generateBase62String();
-                //store data(shorturl, longurl) 
-                const newUrl = new urlModel({
-                    originalUrl: originalUrl,
-                    shortId: shortId
-                });
-            
-                await newUrl.save();
-                
-                //send shorturl to client
-                res.status(201).json(`http://localhost:4000/${shortId}`);
-            }
-        }
-        catch(error){
-            console.log(error);
-            res.status(500).json({message:"Internal Server Error"});
-        }
+        await newUrl.save();
+
+        //send shorturl to client
+        res.status(201).json(`${process.env.REDIRECT_URL}/${shortId}`);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal Server Error" });
     }
+  }
 
-    //if longurl is not a valid.
-    else{
-        res.status(400).json('Please Enter a Valid URL');
-    }
-}
-
+  //if longurl is not a valid.
+  else {
+    res.status(400).json("Please Enter a Valid URL");
+  }
+};
 
 module.exports = {
-    getSpecificUrl,
-    createUrl
-}
+  getSpecificUrl,
+  createUrl,
+};
