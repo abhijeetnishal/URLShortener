41ba879bae7deea24db4e3bac0b13ecdc578d6ed
Sahil Chalke <sahilchalke@Sahils-MacBook-Air.local>
2024-05-11 01:26:06 +0530

    changed port from 4000 to 8080 in setup.sh

--- Setup.sh
+++ Setup.sh
@@ -5,14 +5,14 @@ cp .env.example .env
 echo "NODE_ENV=development" > .env
 echo "CLIENT_PROD_URL=http://localhost:3000" >> .env
 echo "DB_URL=mongodb://localhost:27017" >> .env
-echo "REDIRECT_URL=http://localhost:4000" >> .env
-echo "Port=4000" >>.env
+echo "REDIRECT_URL=http://localhost:8080" >> .env
+echo "Port=8080" >>.env
 
 cd ..
 
 cd client
 npm install
-echo "REACT_APP_SERVER=http://localhost:4000" > .env
+echo "REACT_APP_SERVER=http://localhost:8080" > .env
 
 cd ..
 
