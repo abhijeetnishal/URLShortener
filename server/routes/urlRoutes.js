7f54b72ec663f80a464a305f335310b0b492e22f
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2024-05-13 22:24:32 +0530

    added middleware and fixed getAllUserUrls controller

--- server/routes/urlRoutes.js
+++ server/routes/urlRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { getSpecificUrl, createUrl, getAllUserUrls } = require("../controller/urlController");
-
+const {isAuthenticated} = require("../middlewares/auth")
 const urlRouter = new express.Router();
 
-urlRouter.get("/v1/apis/urls", getAllUserUrls);
+urlRouter.get("/api/v1/urls", isAuthenticated, getAllUserUrls);
 //This route is used to redirect to original url using short url as id
 urlRouter.get("/:shortId", getSpecificUrl);
 //This route is used to create short url
