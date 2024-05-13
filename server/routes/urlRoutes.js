3589c1de3118789466fe9d5bf9bd590c277cfefe
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2024-05-13 14:10:11 +0530

    Created an API endpoint (v1/api/urls) for getting all user urls

--- server/routes/urlRoutes.js
+++ server/routes/urlRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getSpecificUrl, createUrl } = require("../controller/urlController");
+const { getSpecificUrl, createUrl, getAllUserUrls } = require("../controller/urlController");
 
 const urlRouter = new express.Router();
 
+urlRouter.get("/v1/apis/urls", getAllUserUrls);
 //This route is used to redirect to original url using short url as id
 urlRouter.get("/:shortId", getSpecificUrl);
 //This route is used to create short url
