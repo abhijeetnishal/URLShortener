6336f3532cfe7230cf1e2ae248a5966bb20af529
abhijeetnishal <abhijeetnishal@gmail.com>
2024-05-19 16:05:13 +0530

    fix: UI issues

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -1,28 +1,141 @@
-import type { Metadata } from "next";
-import LoginPage from './login-page';
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { GoogleIcon } from "@/components/icons/GoogleIcon";
 
+const LoginPage = () => {
+  const [formValues, setFormValues] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState({ email: "", password: "" });
 
-export const metadata: Metadata = {
-    title: "Login",
-    description: "login for an account",
-};
+  const validateForm = () => {
+    let tempErrors = { email: "", password: "" };
+    let formIsValid = true;
 
-const LoginLayout = (
-    { children, }: { children: React.ReactNode }
-) => {
-    return (
-        <>
-            <div role="status">
-                <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                </svg>
-                <span className="sr-only ">Loading...</span>
+    if (!formValues.email) {
+      formIsValid = false;
+      tempErrors.email = "Please enter your email address.";
+    } else if (!/\S+@\S+\.\S+/.test(formValues.email)) {
+      formIsValid = false;
+      tempErrors.email = "Email address is invalid.";
+    }
+
+    if (!formValues.password) {
+      formIsValid = false;
+      tempErrors.password = "Please enter your password.";
+    } else if (formValues.password.length < 6) {
+      formIsValid = false;
+      tempErrors.password = "Password should have at least 6 characters.";
+    }
+
+    setErrors(tempErrors);
+    return formIsValid;
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues({ ...formValues, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (validateForm()) {
+      console.log(formValues);
+    }
+  };
+
+  return (
+    <div>
+      <div className="relative h-dvh mt-4 bg-gray-100 flex flex-col justify-start py-4 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-2 text-center md:text-3xl text-xl font-extrabold text-gray-900">
+            Welcome back
+          </h2>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-12 p-4 shadow sm:rounded-lg sm:px-10">
+            <button className="w-full mb-4 flex items-center justify-center border border-gray-300 p-3 rounded-lg">
+              <div className="flex flex-row">
+                <GoogleIcon />
+                <div className="ml-4 text-sm font-semibold text-gray-700">
+                  Login with Google
+                </div>
+              </div>
+            </button>
+
+            <div className="flex flex-row justify-start items-center mt-6">
+              <hr className="w-48 border-1 border-gray-300"></hr>
+              <div className="px-2 text-gray-400 font-medium">or</div>
+              <hr className="w-48 border-1 border-gray-300"></hr>
             </div>
-            <LoginPage />
-            {children}
-        </>
-    );
+
+            <form className="space-y-6" onSubmit={handleSubmit} method="POST">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email address
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter your email address"
+                    onChange={handleChange}
+                  />
+                  {errors.email && <p>{errors.email}</p>}
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter your password"
+                    onChange={handleChange}
+                  />
+                  {errors.password && <p>{errors.password}</p>}
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Sign up
+                </button>
+              </div>
+            </form>
+
+            <p className="mt-5 text-center text-sm text-gray-600 max-w">
+              Don&apos;t have an account yet?
+              <Link
+                href="/auth/signup"
+                className="ml-2 font-medium text-blue-600 hover:text-blue-500"
+              >
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default LoginLayout;
+export default LoginPage;
--- client/src/app/auth/signup/page.tsx
+++ client/src/app/auth/signup/page.tsx
@@ -1,27 +1,193 @@
-import type { Metadata } from "next";
-import SignUpPage from "./signup-page";
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-export const metadata: Metadata = {
-    title: "Sign Up",
-    description: "Sign up for an account",
+type FormValues = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 };
 
-const SignUpLayout = (
-    { children, }: { children: React.ReactNode }
-) =>{
-    return (
-        <>
-            <div role="status">
-                <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                </svg>
-                <span className="sr-only">Loading...</span>
+type Errors = {
+  name?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+};
+
+const SignUpPage = () => {
+  const [formValues, setFormValues] = useState<FormValues>({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState<Errors>({});
+
+  const validateForm = () => {
+    let tempErrors: Errors = {};
+
+    // Name validation
+    if (
+      formValues.name.trim().length < 2 ||
+      formValues.name.trim().length > 50
+    ) {
+      tempErrors.name = "Name should have 2 to 50 characters.";
+    }
+
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(formValues.email)) {
+      tempErrors.email = "Invalid email address.";
+    }
+
+    // Password validation
+    if (formValues.password.length < 6) {
+      tempErrors.password = "Password should have at least 6 characters.";
+    }
+
+    // Confirm password validation
+    if (formValues.password !== formValues.confirmPassword) {
+      tempErrors.confirmPassword = "Passwords do not match.";
+    }
+
+    setErrors(tempErrors);
+    return Object.keys(tempErrors).length === 0;
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (validateForm()) {
+      console.log(formValues);
+    }
+  };
+
+  return (
+    <div className="relative h-dvh mt-4 bg-gray-100 flex flex-col justify-start py-4 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-2 text-center md:text-3xl text-xl font-extrabold text-gray-900">
+          Sign up for an account
+        </h2>
+      </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" onSubmit={handleSubmit} method="POST">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Full Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  required
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Enter your full name"
+                  onChange={handleChange}
+                />
+                {errors.name && <p>{errors.name}</p>}
+              </div>
             </div>
-            <SignUpPage />
-            {children}
-        </>
-    );
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Enter your email address"
+                  onChange={handleChange}
+                />
+                {errors.email && <p>{errors.email}</p>}
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Enter your password"
+                  onChange={handleChange}
+                />
+                {errors.password && <p>{errors.password}</p>}
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="confirmPassword"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Confirm Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Confirm your password"
+                  onChange={handleChange}
+                />
+                {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-5 text-center text-sm text-gray-600 max-w">
+            Already have an account?
+            <Link
+              href="/auth/login"
+              className="ml-2 font-medium text-blue-600 hover:text-blue-500"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default SignUpLayout;
+export default SignUpPage;
