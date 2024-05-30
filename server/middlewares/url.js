a6a3cb1dc65d6e0685f63247d54d9d9184e34dc9
abhijeetnishal <abhijeetnishal@gmail.com>
2024-05-30 23:44:34 +0530

    refactor: URL validation

--- server/middlewares/url.js
+++ server/middlewares/url.js
@@ -1,12 +1,12 @@
 const https = require("node:https");
 
-const VALID_STATUS_CODES = new Set(
-  [...Array.from({ length: 100 }, (_, i) => 200 + i), // 2xx codes
+const VALID_STATUS_CODES = new Set([
+  ...Array.from({ length: 100 }, (_, i) => 200 + i), // 2xx codes
   ...Array.from({ length: 100 }, (_, i) => 300 + i), // 3xx codes
-  999 ]
-);
+  999,
+]);
 
-async function checkWebsite(url, retries = 3) {
+async function verifyURL(url, retries = 3) {
   for (let attempt = 1; attempt <= retries; attempt++) {
     try {
       const response = await new Promise((resolve, reject) => {
@@ -42,29 +42,4 @@ async function checkWebsite(url, retries = 3) {
   return false;
 }
 
-const isValidURL = async (req, res, next) => {
-  try {
-    const { originalUrl } = req.body;
-
-    if (!originalUrl) {
-      return res.status(400).json({
-        message: "URL is missing",
-      });
-    }
-    const check = await checkWebsite(originalUrl);
-
-    if (check) {
-      next();
-    } else {
-      return res.status(400).json({
-        message: "url is not valid",
-      });
-    }
-  } catch (err) {
-    return res.status(500).json({
-      message: "Internal server error",
-    });
-  }
-};
-
-module.exports = isValidURL;
+module.exports = verifyURL;
